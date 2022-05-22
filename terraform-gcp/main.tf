693315524db95f1de34a0e821515e44dc796dd6d
ChristianP <c.picon@uniandes.edu.co>
2022-05-21 19:23:32 -0500

    terraform updated outputs.tf created variables.tf added google_compute_network google_compute_firewall google_compute_instance resources added

--- terraform-gcp/main.tf
+++ terraform-gcp/main.tf
@@ -8,14 +8,31 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("keyfile.json")
-  project     = "mlops-3"
-  region      = "us-central1"
-  zone        = "us-central1-c"
+  credentials = "${var.credentials_file}"
+  project     = "${var.project}"
+  region      = "${var.region}"
+  zone        = "${var.zone}"
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+  name = "finapp-network"
+}
+
+resource "google_compute_firewall" "finapp_firewall" {
+  name    = "finapp-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["80", "8080", "1000-2000"]
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000"]
+  }
+
+  source_tags = ["web"]
 }
 
 # Create new storage bucket in the US multi-region
@@ -27,4 +44,21 @@ resource "google_storage_bucket" "data-lake" {
   force_destroy = true
 
   uniform_bucket_level_access = true
+}
+//https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance
+resource "google_compute_instance" "finapp_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
 }
